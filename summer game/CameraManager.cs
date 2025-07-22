604b1cc03c663e04c4b1947e2ada91fd7c520d00
Chris yang <chrisyang2004@outlook.com>
2025-07-21 23:31:34 -0500

    real

---
+++ summer game/CameraManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Microsoft.Xna.Framework;
+using MyMonoGameLibrary.Scenes;
+
+
+namespace summer_game;
+
+public class CameraManager : BehaviorComponent
+{
+    public static CameraManager Instance { get; private set; }
+    
+    private Transform _player;
+    private float _speed = 50f; 
+
+    public override void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            SceneTools.Destroy(Parent);
+        }
+
+            _player = SceneTools.GetGameObject("player").Transform;
+    }
+
+    public override void LateUpdate(GameTime gameTime)
+    {
+        Camera.position.X = _player.position.X;
+
+        float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        if (_player.position.Y <= -6.5)
+        {
+            Camera.position.Y = MathF.Max(-9, Camera.position.Y - (_speed * deltaTime));
+        }
+        else if (_player.position.Y > 3.5)
+        {
+            Camera.position.Y = MathF.Min(9, Camera.position.Y + (_speed * deltaTime));
+        }
+        else
+        {
+            if (Camera.position.Y > 0)
+            {
+                Camera.position.Y = MathF.Max(0, Camera.position.Y - (_speed * deltaTime));
+            }
+            else if (Camera.position.Y < 0)
+            {
+                Camera.position.Y = MathF.Min(0, Camera.position.Y + (_speed * deltaTime));
+            }
+
+        }
+    }
+}
---
+++ summer game/PlayerHealth.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summer_game;
+
+public class PlayerHealth : Health
+{
+    public override int MaxHealth 
+    { 
+        get => base.MaxHealth;
+        set
+        {
+            base.MaxHealth = value;
+            HealthUI.Instance.SetHealth(CurrentHealth, value);
+        } 
+    }
+    public override int CurrentHealth 
+    { 
+        get => base.CurrentHealth;
+        set
+        {
+            base.CurrentHealth = value;
+            HealthUI.Instance.SetHealth(value, MaxHealth);
+
+        }
+    }
+
+    public PlayerHealth(int health) : base(health)
+    {
+    }
+
+    public PlayerHealth(int maxHealth, int currentHealth) : base(maxHealth, currentHealth)
+    {
+    }
+
+    public override void Start()
+    {
+        HealthUI.Instance.SetHealth(CurrentHealth, MaxHealth);
+    }
+
+    public override void Heal(int heal)
+    {
+        base.Heal(heal);
+        HealthUI.Instance.AddHealth(heal, MaxHealth);
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+        HealthUI.Instance.RemoveHealth(damage, MaxHealth);
+    }
+}
